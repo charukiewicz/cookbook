403e9f8dd0c9df634aa27e0564660703e411c1c4
charukiewicz <c.charukiewicz@gmail.com>
2020-05-09 02:38:26 -0500

    added haskell executable via nix-shell example

---
+++ nix/haskell-executable-nix-shell.hs
@@ -0,0 +1,13 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i runghc -p "with import <nixpkgs> { }; haskellPackages.ghcWithPackages (pkgs: with pkgs; [ HTTP tagsoup ])"
+#!nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs-channels/archive/nixos-18.09.tar.gz
+
+import           Network.HTTP
+import           Text.HTML.TagSoup
+
+main = do
+  resp <- Network.HTTP.simpleHTTP (getRequest "http://nixos.org/")
+  body <- getResponseBody resp
+  let tags = filter (isTagOpenName "a") $ parseTags body
+  let tags' = map (fromAttrib "href") tags
+  mapM_ putStrLn $ filter (/= "") tags'
