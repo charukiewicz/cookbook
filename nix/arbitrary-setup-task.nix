d00092f5e240729064bccbc5ed828f0344712482
Christian Charukiewicz <charukiewicz@protonmail.com>
2020-10-23 19:29:56 -0500

    Update arbitrary-setup-task.nix

--- nix/arbitrary-setup-task.nix
+++ nix/arbitrary-setup-task.nix
@@ -1,11 +1,13 @@
-# Registers a systemd unit as a oneshot that runs an
-# arbitrary script. Useful for system setup tasks.
-systemd.services.foo = {
-  script = ''
-    echo hello
-  '';
-  wantedBy = [ 'multi-user.target' ];
-  serviceConfig = {
-    Type = "oneshot";
+{
+  # Registers a systemd unit as a oneshot that runs an
+  # arbitrary script. Useful for system setup tasks.
+  systemd.services.foo = {
+    script = ''
+      echo hello
+    '';
+    wantedBy = [ 'multi-user.target' ];
+    serviceConfig = {
+      Type = "oneshot";
+    };
   };
-};
+}
