770336c2bc00265c6a308dc01b23fcc32ff8f533
Christian Charukiewicz <charukiewicz@protonmail.com>
2021-07-01 12:17:05 -0500

    Update list-nix-store-large-files.sh

--- nix/list-nix-store-large-files.sh
+++ nix/list-nix-store-large-files.sh
@@ -1,1 +1,4 @@
 sqlite3 /nix/var/nix/db/db.sqlite -header -column "select path,narSize/1024/1024,datetime(registrationTime,'unixepoch') from ValidPaths where narSize > (1024*1024*256) order by narSize desc limit 20;"
+
+# With output paths
+nix-store -q --roots /nix/store/<hash>-<pkg>
